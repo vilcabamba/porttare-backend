8d47c2c467c6e02e6c16515636a18e85ddd01372
Mario Andrés Correa <a@macool.me>
2016-07-01 10:05:07 -0500

    hack omniauth callbacks

--- app/controllers/api/auth/omniauth_callbacks_controller.rb
+++ app/controllers/api/auth/omniauth_callbacks_controller.rb
@@ -16,6 +16,24 @@ module Api
         param :provider, String, required: true
         param :auth_origin_url, String, required: true
       end
+
+      # taken from github to make it work
+      # https://github.com/lynndylanhurley/devise_token_auth/blob/1d6dbdc2e5ba2de00264362a8a876dc98585bd0c/app/controllers/devise_token_auth/omniauth_callbacks_controller.rb
+      # intermediary route for successful omniauth authentication. omniauth does
+      # not support multiple models, so we must resort to this terrible hack.
+      def redirect_callbacks
+        # derive target redirect route from 'resource_class' param, which was set
+        # before authentication.
+        devise_mapping = Devise.mappings[:api_auth_user]
+        redirect_route = "#{request.protocol}#{request.host_with_port}/#{devise_mapping.fullpath}/#{params[:provider]}/callback"
+
+        # preserve omniauth info for success route. ignore 'extra' in twitter
+        # auth response to avoid CookieOverflow.
+        session['dta.omniauth.auth'] = request.env['omniauth.auth'].except('extra')
+        session['dta.omniauth.params'] = request.env['omniauth.params']
+
+        redirect_to redirect_route
+      end
     end
   end
 end
