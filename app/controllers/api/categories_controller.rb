496bc2582cbe5bba5bc5e1633d996f54b83dada5
Mario Andrés Correa <a@macool.me>
2016-09-01 19:01:49 -0500

    document api endpoint

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -4,6 +4,25 @@ module Api
 
     respond_to :json
 
+    api :GET,
+        "/categories",
+        "List provider categories including their providers"
+    example %q{{
+  "categories":[
+    {
+      "id":2,
+      "titulo":"Alimentos preparados",
+      "imagen":"https://robohash.org/veniameanostrum.png?size=400x600\u0026set=set1",
+      "descripcion":"Carry mustache twee brooklyn.",
+      "providers":[
+        {
+          "id":2,
+          "razon_social":"Perea S.L."
+        }
+      ]
+    }
+  ]
+}}
     def index
       @categories = public_scope.order(:titulo)
     end
