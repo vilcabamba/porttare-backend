9c581e0267bde0abfdbf7ea400a2272a2cdfb8b0
Mario Andrés Correa <a@macool.me>
2016-09-21 10:17:56 -0500

    attempt to fix migrations

--- db/migrate/20160920121753_add_nombre_establecimiento_to_provider_profile.rb
+++ db/migrate/20160920121753_add_nombre_establecimiento_to_provider_profile.rb
@@ -2,9 +2,9 @@ class AddNombreEstablecimientoToProviderProfile < ActiveRecord::Migration
   def up
     # otherwise new not-null fields will complain
     say_with_time "WARNING: wiping all provider items, provider clients and provider profiles" do
-      ProviderItem.destroy_all
-      ProviderClient.destroy_all
-      ProviderProfile.destroy_all
+      ProviderItem.unscoped.destroy_all
+      ProviderClient.unscoped.destroy_all
+      ProviderProfile.unscoped.destroy_all
     end
 
     add_column :provider_profiles,
