548ad438382bf4f523a07bde1f58b96c05bb0972
Mario Andrés Correa <a@macool.me>
2016-10-28 16:20:20 -0500

    update policies

--- app/policies/customer_profile_policy.rb
+++ app/policies/customer_profile_policy.rb
@@ -1,13 +1,2 @@
 class CustomerProfilePolicy < ApplicationPolicy
-  def permitted_attributes
-    [
-      :name,
-      :email,
-      :password,
-      customer_profile_attributes: [
-        :fecha_de_nacimiento,
-        :ciudad
-      ]
-    ]
-  end
 end
