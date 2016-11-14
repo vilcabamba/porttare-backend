d9248f346c771c56f46fb2a6569196eb78400636
Fabricio Flores <fabrifloresg@gmail.com>
2016-11-14 17:19:44 +0100

    minor changes

--- app/uploaders/provider_category_image_uploader.rb
+++ app/uploaders/provider_category_image_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class ProviderCategoryImageUploader < ApplicationUploader
-  def default_url(*args)
-    "/images/fallback/" + [model.titulo.parameterize, ".jpg"].compact.join('')
+  def default_url
+    "/images/provider_category/defaults/" + [model.titulo.parameterize, ".jpg"].compact.join('')
   end
 end
