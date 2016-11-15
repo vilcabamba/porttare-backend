d4677bda71a3f5e8e62ed1a8317e9956604eaf0b
Mario Andrés Correa <a@macool.me>
2016-11-14 19:21:50 -0500

    provide asset host in category images

--- app/uploaders/provider_category_image_uploader.rb
+++ app/uploaders/provider_category_image_uploader.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 class ProviderCategoryImageUploader < ApplicationUploader
   def default_url
-    "/images/provider_category/defaults/" + [model.titulo.parameterize, ".jpg"].compact.join('')
+    uri = "/images/provider_category/defaults/" + model.titulo.parameterize + ".jpg"
+    asset_host + uri
   end
 end
