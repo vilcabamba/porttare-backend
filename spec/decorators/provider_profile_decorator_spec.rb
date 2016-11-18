5ec34101f9b305e9b0890588c242a0b0d980bc53
Mario Andrés Correa <a@macool.me>
2016-11-17 22:39:20 -0500

    some specs

--- spec/decorators/provider_profile_decorator_spec.rb
+++ spec/decorators/provider_profile_decorator_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe ProviderProfileDecorator,
          type: :decorator do
+  describe "model" do
+    subject { build_stubbed(:provider_profile).decorate }
+    it { is_expected.to be_a(described_class) }
+  end
 end
