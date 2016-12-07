0a03ae4a03851317ee7b963b7384d1dfb3b92528
Mario Andrés Correa <a@macool.me>
2016-12-07 02:49:25 -0500

    use selectize for admin privileges

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -5,4 +5,5 @@
 //= require bootstrap
 //= require imagesloaded.pkgd.min.js
 //= require masonry.pkgd.min.js
+//= require selectize.standalone.js
 //= require_tree ./admin
---
+++ app/assets/javascripts/admin/selectize.js.coffee
@@ -0,0 +1,2 @@
+$(document).on "ready page:load", ->
+  $(".js-selectize").selectize()
