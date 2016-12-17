a6a67be3c4cb8c9b7eead1d8c4ce46e2a60db25b
Mario Andrés <a@macool.me>
2016-12-17 06:17:11 -0500

    [hotfix] do not fail miserably when readding without previous `observaciones` (#87)
    * [hotfix] do not fail miserably when readding without previous `observaciones`
    
    addresses https://pangi.shiriculapo.com/apps/577670c995ca4e3219000021/problems/585486b695ca4e44a000000e
    
    * Update customer_order_item.rb
    
    * Update customer_order_item.rb

--- app/models/customer_order_item.rb
+++ app/models/customer_order_item.rb
@@ -71,7 +71,8 @@ class CustomerOrderItem < ActiveRecord::Base
     def readd_from_attributes(parameters)
       self.cantidad += parameters[:cantidad].to_i
       if parameters[:observaciones].present?
-        self.observaciones += "\n" + parameters[:observaciones]
+        self.observaciones =
+          self.observaciones.to_s + "\n" + parameters[:observaciones]
       end
     end
 
