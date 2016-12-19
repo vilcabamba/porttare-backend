1afc27eb0cb9f9ee5fa8a0d2f9874647bbd30830
Mario Andrés Correa <a@macool.me>
2016-12-19 08:59:52 -0500

    prefixes for dj

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,8 @@ set :slackistrano, {
   webhook: 'https://hooks.slack.com/services/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'
 }
 
+set :delayed_job_prefix, 'porttare-backend'
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :branch, ENV['BRANCH'] || 'production'
