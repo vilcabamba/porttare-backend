6bd0ab61a61ce759036960c533df98d17c42ced9
Mario Andrés Correa <a@macool.me>
2016-12-25 12:28:54 -0500

    addressing specs

--- spec/requests/api/categories/providers/provider_item_categories_spec.rb
+++ spec/requests/api/categories/providers/provider_item_categories_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe Api::ProvidersController,
     }
     let(:provider_item) {
       create :provider_item,
+             :en_stock,
              provider_profile: provider_profile
     }
 
