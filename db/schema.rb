b57127530e407fd16818d6223477d0d9bcb0cf92
Mario Andrés Correa <a@macool.me>
2017-01-05 23:30:52 -0500

    generate default office weekdays

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106035331) do
+ActiveRecord::Schema.define(version: 20170106042909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
