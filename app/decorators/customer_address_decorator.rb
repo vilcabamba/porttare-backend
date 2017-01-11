4edd7e274e0ae9ab15a51df824407161a716e841
Mario Andrés Correa <a@macool.me>
2017-01-11 04:06:57 -0500

    enhance UI

--- app/decorators/customer_address_decorator.rb
+++ app/decorators/customer_address_decorator.rb
@@ -2,6 +2,7 @@ class CustomerAddressDecorator < GenericResourceDecorator
   def static_map_image
     GoogleStaticMap.new(
       zoom: 16,
+      scale: 2,
       size: "600x300",
       markers: "#{lat},#{lon}"
     ).to_s
