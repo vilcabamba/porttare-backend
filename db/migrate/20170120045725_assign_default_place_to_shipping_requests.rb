82bdd2defa854f8769c7e4d357c325e122f225b8
Fabricio Flores <fabrifloresg@gmail.com>
2017-01-26 20:32:15 +0100

    Add fallback when no places in db

--- db/migrate/20170120045725_assign_default_place_to_shipping_requests.rb
+++ db/migrate/20170120045725_assign_default_place_to_shipping_requests.rb
@@ -1,9 +1,11 @@
 class AssignDefaultPlaceToShippingRequests < ActiveRecord::Migration
   def up
     say_with_time "making all shipping requests belong to first place" do
-      place_id = Place.first.id
-      ShippingRequest.find_each do |shipping_request|
-        shipping_request.update!(place_id: place_id)
+      if Place.first
+        place_id = Place.first.id
+        ShippingRequest.find_each do |shipping_request|
+          shipping_request.update!(place_id: place_id)
+        end
       end
     end
 
