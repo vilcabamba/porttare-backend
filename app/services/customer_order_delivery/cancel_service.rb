c24cfd66782ce4bde178884732db8c476a87862d
Mario Andrés Correa <a@macool.me>
2017-02-12 20:42:36 -0500

    user cant cancel old deliveries

--- app/services/customer_order_delivery/cancel_service.rb
+++ app/services/customer_order_delivery/cancel_service.rb
@@ -6,13 +6,27 @@ class CustomerOrderDelivery < ActiveRecord::Base
 
     def perform
       if can_cancel?
-        @customer_order_delivery.update!(
-          status: :canceled
-        )
+        cancel!
+      else
+        add_errors
+        false
       end
     end
 
     private
+
+    def cancel!
+      @customer_order_delivery.update!(
+        status: :canceled
+      )
+    end
+
+    def add_errors
+      @customer_order_delivery.errors.add(
+        :base,
+        :cant_cancel_old_delivery
+      )
+    end
 
     def can_cancel?
       responded_at = @customer_order_delivery.provider_responded_at
--- spec/requests/api/customers/orders/cancel_spec.rb
+++ spec/requests/api/customers/orders/cancel_spec.rb
@@ -4,22 +4,22 @@ RSpec.describe Api::Customer::OrdersController,
                type: :request do
   let(:user) { create :user, :customer }
   before { login_as user }
+  let(:customer_order) {
+    create :customer_order,
+           :submitted,
+           customer_profile: user.customer_profile
+  }
 
   describe "I can cancel a recent order" do
     it {
-      recent_order = create(
-        :customer_order,
-        :submitted,
-        customer_profile: user.customer_profile
-      )
       delivery = create(
         :customer_order_delivery,
-        customer_order: recent_order,
+        customer_order: customer_order,
         status: :accepted,
         provider_responded_at: Time.now
       )
       post_with_headers(
-        "/api/customer/orders/#{recent_order.id}/deliveries/#{delivery.id}/cancel"
+        "/api/customer/orders/#{customer_order.id}/deliveries/#{delivery.id}/cancel"
       )
       response_delivery = JSON.parse(
         response.body
@@ -30,16 +30,17 @@ RSpec.describe Api::Customer::OrdersController,
 
   describe "I can't cancel an old order" do
     it {
-      recent_order = create(
-        :customer_order,
-        :submitted,
-        customer_profile: user.customer_profile
+      delivery = create(
+        :customer_order_delivery,
+        customer_order: customer_order,
+        status: :accepted,
+        provider_responded_at: 15.minutes.ago
       )
       post_with_headers(
-        "/api/customer/orders/#{recent_order.id}/cancel"
+        "/api/customer/orders/#{customer_order.id}/deliveries/#{delivery.id}/cancel"
       )
-      response_order = JSON.parse(response.body).fetch("customer_order")
-      expect(response_order["status"]).to eq("canceled")
+      response_errors = JSON.parse(response.body).fetch("errors")
+      expect(response_errors["base"].length).to eq(1)
     }
   end
 end
