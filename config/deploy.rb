2aa7a8b9bc4cb8438797bb4c8985c828f585d64a
Mario Andrés Correa <a@macool.me>
2017-06-23 21:17:56 -0500

    legacy passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,6 +44,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 set :rbenv_ruby, '2.3.0'
+set :passenger_restart_with_touch, true
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
